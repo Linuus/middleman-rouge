a7102fb8e5dab622c901c7c009eb6a83ee45bd9c
Linus Pettersson <linus.pettersson@gmail.com>
2013-03-11 19:39:52 +0100

    Fixes

--- lib/middleman-rouge.rb
+++ lib/middleman-rouge.rb
@@ -1,8 +1,7 @@
 require "middleman-core"
-require "redcarpet"
 require "middleman-rouge/version"
 
 ::Middleman::Extensions.register(:rouge_syntax) do
   require "middleman-rouge/extension"
-  ::Middleman::MiddlemanRouge
+  ::Middleman::Rouge
 end
--- lib/middleman-rouge/extension.rb
+++ lib/middleman-rouge/extension.rb
@@ -2,8 +2,10 @@ module Middleman
   module Rouge
     class << self
       def registered(app)
+        require "redcarpet"
+        require "rouge"
         require 'rouge/plugins/redcarpet'
-        Redcarpet::Render::HTML.send :include, Rouge::Plugins::Redcarpet
+        Redcarpet::Render::HTML.send :include, ::Rouge::Plugins::Redcarpet
       end
       alias :included :registered
     end
